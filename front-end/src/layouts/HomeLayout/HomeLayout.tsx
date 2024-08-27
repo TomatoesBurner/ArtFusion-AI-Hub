dd9e796dbbf3beedf7faf1c7ebeeaafd0c12c60b
thogb <132250429+thogb@users.noreply.github.com>
2024-08-27 23:16:01 +0800

    Update HomeLayout.tsx
    fix typo

--- front-end/src/layouts/HomeLayout/HomeLayout.tsx
+++ front-end/src/layouts/HomeLayout/HomeLayout.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
-            children
+            {children}
         </div>
     );
 }
