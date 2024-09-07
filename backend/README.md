0230be1e32db92c6b76f77116f83243acbf917cd
Jesssieeee <jessiyang03@gmail.com>
2024-09-07 18:44:25 +0800

    MongoDb Atlas setup 1. Set up our DevEnv database on the Atlas platform 2. Build connection between our application and Atlas(first testing is done!)

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,9 @@
 ## Project Start
-### Developing Environments
+### How initial backend project
+#### Developing Environments
 1.npm run start:dev
-### Production Environments
+#### Production Environments
 2.npm run start:prod
+
+### MongoDB Altas
+1.password and connnection have update on the `config.env`
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 /*****************************************************
  *                 SERVER STARTUP
  *****************************************************/
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'})
-
 const app = require('./app')
+
+dotenv.config({path:'./config.env'})
+console.log(process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB)
+    .then(() => console.log('DB connection successful!'))
+    .catch(err => console.error('DB connection error:', err));
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
