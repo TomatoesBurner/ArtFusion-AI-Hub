f0d334b992cacd1036ce8b4e89812a9d01d5906e
Tao Hu <taohu2222@gmail.com>
2024-09-19 01:15:28 +0800

    created Video prompt schema

---
+++ backend/types/soundTrackExtensionTyps.js
@@ -0,0 +1,10 @@
+export const SOUND_TRACK_EXTENSION = {
+    MP3: "mp3",
+    WAV: "wav",
+    FLAC: "flac",
+    OGG: "ogg",
+};
+
+export const SOUND_TRACK_EXTENSION_VALUES = Object.values(
+    SOUND_TRACK_EXTENSION
+);
