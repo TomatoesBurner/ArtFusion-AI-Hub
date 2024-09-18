586ded70f0a03cdb682a1c051bb49ea23a932a51
Tao Hu <taohu2222@gmail.com>
2024-09-19 00:52:14 +0800

    Added image prompt schema.

---
+++ backend/models/imagePrompt.js
@@ -0,0 +1,60 @@
+import { Schema } from "mongoose";
+import { IMAGE_EXTENSION_VALUES } from "../types/imageExtensionTypes";
+import { IMAGE_PROMPT_MODEL_VALUES } from "../types/imageModelPromptTypes";
+
+const ImagePromptInputSchema = new Schema(
+    {
+        filters: {
+            width: Number,
+            height: Number,
+            dpi: Number,
+            aspectRatio: String,
+        },
+        message: String,
+        fullMessage: String,
+        model: {
+            type: String,
+            enum: IMAGE_PROMPT_MODEL_VALUES,
+        },
+    },
+    { _id: false }
+);
+
+const ImagePromptResponseSchema = new Schema(
+    {
+        originalImageUrl: String,
+        extension: {
+            type: String,
+            enum: IMAGE_EXTENSION_VALUES,
+        },
+    },
+    { timestamps: true }
+);
+
+const ArgumentImagePromptResponseSchema = new Schema(
+    {
+        extension: {
+            type: String,
+            enum: IMAGE_EXTENSION_VALUES,
+        },
+        // TODO: If need change later
+        filters: Schema.Types.Mixed,
+        createdBy: { type: mongoose.Types.ObjectId, ref: "User" },
+    },
+    { timestamps: true }
+);
+
+const ImagePromptSchema = new Schema(
+    {
+        input: ImagePromptInputSchema,
+        response: ImagePromptResponseSchema,
+        argumentResponses: [ArgumentImagePromptResponseSchema],
+        promptSpaceId: { type: mongoose.Types.ObjectId, ref: "PromptSpace" },
+        createdBy: { type: mongoose.Types.ObjectId, ref: "User" },
+    },
+    { timestamps: true }
+);
+
+const ImagePrompt = mongoose.model("ImagePrompt", ImagePromptSchema);
+
+export default ImagePrompt;
---
+++ backend/types/imageModelPromptTypes.js
@@ -0,0 +1,12 @@
+export const IMAGE_PROMPT_MODEL = {
+    Animate: "animate",
+    AbstractArt: "abstract art",
+    OilPaintingStyle: "oil painting style",
+    Sketch: "sketch",
+    Cyberpunk: "cyberpunk",
+    RetroStyle: "retro style",
+    RococoStyle: "rococo style",
+    Realism: "realism",
+};
+
+export const IMAGE_PROMPT_MODEL_VALUES = Object.values(IMAGE_PROMPT_MODEL);
