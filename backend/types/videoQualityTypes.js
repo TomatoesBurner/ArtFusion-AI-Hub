f2d4b3136d54820aa44fc77837b6676c76b6121b
Tao Hu <taohu2222@gmail.com>
2024-09-19 01:15:28 +0800

    created Video prompt schema

---
+++ backend/types/videoQualityTypes.js
@@ -0,0 +1,8 @@
+export const VIDEO_QUALITY = {
+    LOW: "low",
+    MEDIUM: "medium",
+    HIGH: "high",
+    ULTRA_HIGH: "ultra high",
+};
+
+export const VIDEO_QUALITY_VALUES = Object.values(VIDEO_QUALITY);
