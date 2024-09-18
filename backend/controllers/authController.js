8073a4b8d688a88858cfeca2356e8ece1811f0bb
TomatoesBurner <max683879@hotmail.com>
2024-09-18 18:03:41 +0800

    Adjust email content

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModels');
 const AppError = require('./../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const { promisify } = require('util');
-const sendEmail = require('./../utils/email')
+const sendEmail = require('./../utils/email');
 
 
 const signToken = id => {
@@ -89,22 +89,26 @@ exports.protect = catchAsync(async (req, res, next) => {
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
     // 1) Get user based on POSTed email
-    const user = await User.findOne({
-        email: req.body.email
-    });
+    const user = await User.findOne({ email: req.body.email });
     if (!user) {
         return next(new AppError('There is no user with email address.', 404));
     }
+
     // 2) Generate the random reset token
     const resetToken = user.createPasswordResetToken();
     await user.save({ validateBeforeSave: false });
 
     // 3) Send it to user's email
+    const resetURL = `${req.protocol}://your-frontend-domain.com/reset-password?token=${resetToken}`;
+
+    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
     try {
-        const resetURL = `${req.protocol}://${req.get(
-            'host'
-        )}/api/v1/users/resetPassword/${resetToken}`;
-        await new Email(user, resetURL).sendPasswordReset();
+        await sendEmail({
+            email: user.email,
+            subject: 'Your password reset token (valid for 10 min)',
+            message
+        });
 
         res.status(200).json({
             status: 'success',
@@ -112,11 +116,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
         });
     } catch (err) {
         user.passwordResetToken = undefined;
-        user.passwordResetExpire = undefined;
+        user.passwordResetExpires = undefined;
         await user.save({ validateBeforeSave: false });
-
         return next(
-            new AppError('There was an error sending the email. Try again later!', 500)
+            new AppError('There was an error sending the email. Try again later!'),
+            500
         );
     }
 });
