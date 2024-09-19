c96695cf938c011d1d8eff2126f8b30b19fdac8d
Yali Ou <24074405@student.uwa.edu.au>
2024-09-20 04:41:40 +0800

    Adjust the ui

--- front-end/src/app/(home)/mode/mode_selection.tsx
+++ front-end/src/app/(home)/mode/mode_selection.tsx
@@ -1,49 +1,51 @@
-import React from 'react';
-import Link from 'next/link';
-import { Grid, Button, Typography, Box } from '@mui/material';
+import React from "react";
+import Link from "next/link";
+import { Grid, Button, Typography, Box } from "@mui/material";
+import { create } from "@mui/material/styles/createTransitions";
 
 const mode_selection = () => {
   return (
     <Box
       sx={{
-        height: '100vh',
-        backgroundColor: 'black',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        color: 'white',
+        height: "100vh",
+        backgroundColor: "black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        color: "white",
       }}
     >
-      <Grid container spacing={3} justifyContent="center">
+      <Grid container spacing={1} justifyContent="center">
         {/* Modes Section */}
         <Grid item xs={12} md={5}>
           <Box
             sx={{
-              height: '300px',
-              backgroundColor: 'black',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              position: 'relative',
-              flexDirection: 'column',
-              border: '2px solid white',
-              padding: '20px',
-              borderRadius: '15px',
+              height: "300px",
+              backgroundColor: "black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
+              flexDirection: "column",
+              padding: "20px",
+              borderRadius: "15px",
             }}
           >
             <img src="/images/image_gif.gif" alt="Image Mode" width="100%" />
             {/* Image button */}
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: 'white',
-                color: 'black',
-                marginTop: '20px',
-              }}
-            >
-              Start Image
-            </Button>
+            <Link href="./images/create">
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "white",
+                  color: "black",
+                  marginTop: "20px",
+                }}
+              >
+                Start Image
+              </Button>
+            </Link>
           </Box>
         </Grid>
 
@@ -51,30 +53,31 @@ const mode_selection = () => {
         <Grid item xs={12} md={5}>
           <Box
             sx={{
-              height: '300px',
-              backgroundColor: 'black',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              position: 'relative',
-              flexDirection: 'column',
-              border: '2px solid white',
-              padding: '20px',
-              borderRadius: '15px',
+              height: "300px",
+              backgroundColor: "black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
+              flexDirection: "column",
+              padding: "20px",
+              borderRadius: "15px",
             }}
           >
             <img src="/images/video_gif.gif" alt="Video Mode" width="100%" />
             {/* Video button */}
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: 'white',
-                color: 'black',
-                marginTop: '20px',
-              }}
-            >
-              Start Video
-            </Button>
+            <Link href="./videos/create">
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "white",
+                  color: "black",
+                  marginTop: "20px",
+                }}
+              >
+                Start Video
+              </Button>
+            </Link>
           </Box>
         </Grid>
       </Grid>
