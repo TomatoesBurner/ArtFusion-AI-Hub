96f2258da9b8a8c95961b8dda31a0d80234e4dda
Yali Ou <24074405@student.uwa.edu.au>
2024-10-13 10:25:39 +0800

    Add DashBoardLayout component and add dynamic app bar height

--- front-end/src/store/slices/imagesSlice.ts
+++ front-end/src/store/slices/imagesSlice.ts
@@ -1,4 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {
+  ImagePromptDto,
+  ImagePromptInputFilterDto,
+} from "@/dtos/ImagePromptDto";
+import { aspectRatios } from "@/utils/constant";
+import { PaginationResponseDto } from "@/dtos/PaginationResponseDto";
 
 // TODO: decide where this really comes from
 export enum ImageModel {
@@ -15,36 +21,33 @@ export enum ImageModel {
 /**
  * Represent the filter type that user can set in the image prompt page
  */
-type ImagePromptFilter = {
-  width: number;
-  height?: number;
-  // TODO: more
-};
+type ImagePromptFilter = ImagePromptInputFilterDto;
 
 /**
  * Represents one single prompt
  */
-type ImagePrompt = {
-  id: string;
-  text: string;
-  imageUrl: string;
-  // TODO: the definition against the API
-};
+type ImagePrompt = ImagePromptDto;
 
 type ImageSliceState = {
   model: ImageModel;
   filter: ImagePromptFilter;
   // TODO: more
   prompts: ImagePrompt[];
+  hasNextPage: boolean;
+  cursor: string | null;
 };
 
 const initialState: ImageSliceState = {
   model: ImageModel.Animate,
   filter: {
-    width: 0,
-    height: 0,
+    width: 540,
+    height: 540,
+    aspectRatio: aspectRatios[0],
+    dpi: 300,
   },
   prompts: [],
+  hasNextPage: false,
+  cursor: null,
 };
 
 const slice = createSlice({
@@ -61,12 +64,24 @@ const slice = createSlice({
       state.filter = {
         ...state.filter,
         ...filter,
-        ...action.payload.filter,
       };
     },
 
     addPrompts(state, action: { payload: { prompts: ImagePrompt[] } }) {
-      state.prompts = action.payload.prompts;
+      state.prompts = [...state.prompts, ...action.payload.prompts];
+    },
+
+    addPromptToFront(state, action: { payload: { prompt: ImagePrompt } }) {
+      state.prompts = [action.payload.prompt, ...state.prompts];
+    },
+
+    setPagination(
+      state,
+      action: { payload: { pagination: PaginationResponseDto } }
+    ) {
+      const { cursor, hasNext } = action.payload.pagination;
+      state.cursor = cursor || null;
+      state.hasNextPage = hasNext || false;
     },
 
     clearState() {
