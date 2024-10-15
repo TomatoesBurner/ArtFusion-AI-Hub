f614e4167b303ebdb80447bb601c9eca0c6f2d13
“TomatoesBurner” <max683879@hotmail.com>
2024-10-15 13:47:27 +0800

    debug:delete useless route

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -50,18 +50,6 @@ router.patch(
     authController.protect,
     userController.updateProfile
 );
-
 router.post("/check-username", userController.isUserNameDuplicate);
-
-router
-    .route("/")
-    .get(userController.getAllUsers)
-    .post(userController.createUser);
-
-router
-    .route("/:id")
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
 
 module.exports = router;
